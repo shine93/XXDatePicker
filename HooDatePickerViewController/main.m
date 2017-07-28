414377b4d523110e66e2c22aa486a916c708de1c
wmx <weimengxue@adesk.com>
2017-07-28 12:12:24 -0700

    Initial Commit

---
+++ HooDatePickerViewController/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HooDatePickerViewController
+//
+//  Created by xhkj on 2017/7/28.
+//  Copyright © 2017年 adesk.wmx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
